a7322bf43100aae1f0ee84c73621e14cc7c3d780
Quintero <zabaladuran123@gmail.com>
2025-04-27 19:54:06 -0500

    arreglos de errores en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import SignUpPage from "./componentes/pages/sign-up-page";
 import HomePage from "./componentes/pages/home-page";
 import { DashboardPage } from "./componentes/pages";
 import { PermitirUsuarioAutenticado } from "./componentes/navegacion";
-import { BlockearUsuarioAutenticado } from "./componentes/navegacion";
 import NotFoundPage from "./componentes/pages/not-found";
 import { ProveedoresApp } from "./contexto/";
 
@@ -12,21 +11,19 @@ function App() {
   return (
     <ProveedoresApp>
       <Routes>
+        {/* Rutas públicas */}
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
-        {/* Protege rutas privadas con "PermitirUsuarioAutenticado" */}
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+
+        {/* Rutas privadas protegidas */}
         <Route element={<PermitirUsuarioAutenticado to="/sign-in" />}>
           <Route path="/dashboard/home" element={<DashboardPage />} />
         </Route>
-        {/* Protege las rutas de SignIn y SignUp con "BlockearUsuarioAutenticado" */}
 
-        {/* <Route element={<BlockearUsuarioAutenticado to="/dashboard/home" />}> </Route> */}
-
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
         {/* Ruta para páginas no encontradas */}
         <Route path="*" element={<NotFoundPage />} />
-
       </Routes>
     </ProveedoresApp>
   );
